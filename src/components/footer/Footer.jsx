385f65e8f3ca07900325ee41814d10fb70e75d1c
Hayotbek-Turgunov <hayotbekturgunov01@gmail.com>
2026-04-27 23:33:15 +0500

    banne code start

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,14 +4,6 @@ const Footer = () => {
   return (
     <div>
       <h1>hello</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt et
-        dolores sint rem. Tempora voluptatum, deserunt, iusto esse autem amet
-        iste ducimus corrupti impedit dolorem aliquam. Reiciendis sunt aliquam
-        temporibus praesentium, quos nihil maxime iste dolorum nostrum incidunt
-        aspernatur nisi quam voluptates obcaecati facere doloribus iure omnis
-        nulla aut ipsum?
-      </p>
     </div>
   );
 };
