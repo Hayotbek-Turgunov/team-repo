49042274582b7a0b21d9d5a99b58de9ae7c9ed02
Turgunov Dostonbek <turgunovdostonbek@gmail.com>
2026-04-27 23:34:45 +0500

    Hello

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,20 @@ const Footer = () => {
   return (
     <div>
       <h1>hello</h1>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, ex
+        commodi minus, minima dolores nisi necessitatibus voluptate molestias
+        accusantium molestiae sed alias aspernatur nam vel cumque nihil?
+        Repudiandae voluptatum magnam laborum quae, amet debitis sed!
+        Repudiandae ipsum est libero iure veniam incidunt, esse, voluptas
+        voluptatum dolore rem voluptatibus quisquam blanditiis sint minima
+        reiciendis vitae. Cum, expedita iste nam voluptate temporibus, amet
+        illum error quod laborum, a ipsum at architecto! Alias nostrum
+        consequuntur quam, laudantium voluptas ipsum neque minus quibusdam totam
+        possimus corporis obcaecati et at? Harum sapiente, dolorum nihil
+        explicabo animi dolorem dicta facere doloremque quidem, voluptatum
+        veniam, id saepe!
+      </h1>
     </div>
   );
 };
