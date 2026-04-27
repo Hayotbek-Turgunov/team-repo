498f9262685051c96eaa8bab1b7dc046fff3d59f
Turgunov Dostonbek <turgunovdostonbek@gmail.com>
2026-04-27 23:26:53 +0500

    Hello

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,19 +3,7 @@ import React from "react";
 const Banner = () => {
   return (
     <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
-        ullam in consequuntur est magnam alias, pariatur quaerat cupiditate
-        natus modi, perferendis quia quas consequatur ratione minima autem
-        veritatis aliquid sunt repellat sed quidem qui impedit. Impedit magnam,
-        repellat dolore accusamus nobis accusantium mollitia similique
-        temporibus tenetur architecto harum eaque animi saepe fuga? Facere saepe
-        quas, sequi alias accusamus consequatur maxime quis necessitatibus
-        veniam. Odit, laudantium! Id quam obcaecati pariatur vero praesentium
-        culpa vitae at, soluta enim. Mollitia fugit dolore eos voluptas
-        temporibus magni adipisci iure unde dolor minima a ad nulla quasi ipsa
-        minus accusantium veritatis doloribus, laborum voluptatem voluptates.
-      </p>
+      <h2>Hello</h2>
     </div>
   );
 };
