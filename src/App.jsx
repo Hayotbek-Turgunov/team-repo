05fcdb0d91cd8c5586008cfdc7f75e954a369903
Hayotbek-Turgunov <hayotbekturgunov01@gmail.com>
2026-04-27 23:09:10 +0500

    repo-start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Banner from "./components/banner/Banner";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
